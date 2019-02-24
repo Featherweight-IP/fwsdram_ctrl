60b697283a4901a46d320b9ff895f29959a4c0ee
Matthew Ballance <matt.ballance@gmail.com>
2019-02-24 15:32:36 -0800

    filling out project structure

---
+++ etc/fwsdram_ctrl_env.sh
@@ -0,0 +1,17 @@
+#****************************************************************************
+#* fwsdram_ctrl_env.sh
+#*
+#* Environment setup script for fwsdram_ctrl
+#****************************************************************************
+
+
+etc_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd)"
+rootdir=`cd $etc_dir/.. ; pwd`
+
+
+
+# Add a path to the simscripts directory
+export PATH=$rootdir/packages/simscripts/bin:$PATH
+
+# Force the PACKAGES_DIR
+export PACKAGES_DIR=$rootdir/packages
